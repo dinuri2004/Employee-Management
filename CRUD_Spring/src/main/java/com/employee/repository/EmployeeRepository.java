675f77c72abeef49dc96e2e42b4e3aa10fa98e11
Dinuri2004 <dinuri.chamindi04@gmail.com>
2026-03-10 22:13:34 +0530

    Edit who can view the data

--- CRUD_Spring/src/main/java/com/employee/repository/EmployeeRepository.java
+++ CRUD_Spring/src/main/java/com/employee/repository/EmployeeRepository.java
@@ -1,10 +1,11 @@
 package com.employee.repository;
 
-import com.employee.entity.Employee;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.employee.entity.Employee;
 
 @SuppressWarnings("unused")
 @Repository
